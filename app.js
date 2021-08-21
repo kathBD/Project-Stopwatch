e2f83b75abc92bd324c67868ed1725910013b64c
kathBD <86696439+kathBD@users.noreply.github.com>
2021-08-21 13:41:07 -0500

    Version-2

--- app.js
+++ app.js
@@ -1,53 +1,33 @@
-
-//Variable cronometro
 let cronometro;
 
-//llamo a mis clases que estan en el html
-let s = document.querySelector('.seconds');
-let m = document.querySelector('.minutes');
-let mls = document.querySelector('.miliseconds');
-
-
-//Me vuelva a cargar mi cronometro y me inicialize todo en cero
-function carga() {
-    let contadorMinutes = 0;
-    let contadorSeconds = 0;
-    let contadorMiliseconds = 0;
-
-    //setInterval ejecuta una función o un fragmento de código de forma repetitiva 
-    //cada vez que termina el periodo de tiempo determinado
-    cronometro = setInterval(
-        function () {
-            if (contadorMiliseconds == 60) {
-                contadorMiliseconds = 0;
-                contadorSeconds++
-                s.innerHTML = contadorSeconds; //Nos asigna este valor en el html 
-
-                if (contadorSeconds == 60) {
-                    contadorSeconds = 0;
-                    contadorMinutes++
-                    m.innerHTML = contadorMinutes //Nos asigna este valor en el html 
-                }
-            }
-            mls.innerHTML = contadorMiliseconds;
-            contadorMiliseconds++
-
-        }, 17); //la velocidad en que debe actualizar,  //1 segundo es igual a 1000 mls, 1000 mls se divive entre 60segundos
-}
-
-//Esta funcion es para detener el bucle ¨clearInterval¨
+//Para detener el bucle
 function detenerse() {
-    clearInterval(cronometro);
+  clearInterval(cronometro);
 }
 
+//
+function carga() {
+  let contadorMinutes = 0;
+  let contadorSeconds = 0;
+  let contadorMiliseconds = 0;
 
+  let m = document.querySelector(".minutes");
+  let s = document.querySelector(".seconds");
+  let mls = document.querySelector(".miliseconds");
 
+  cronometro = setInterval(function () {
+    if (contadorMiliseconds == 60) {
+      contadorMiliseconds = 0;
+      contadorSeconds++;
+      s.innerHTML = contadorSeconds;
 
-
-
-
-
-
-
-
-
+      if (contadorSeconds == 60) {
+        contadorSeconds = 0;
+        contadorMinutes++;
+        m.innerHTML = contadorMinutes;
+      }
+    }
+    mls.innerHTML = contadorMiliseconds;
+    contadorMiliseconds++;
+  }, 17);
+}
